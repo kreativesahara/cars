51df633c30955194ebc455adcda7722f14dbff95
kreativesahara <technologieslensmen@gmail.com>
2024-10-03 10:36:08 +0300

    Update: the db was well migrated with the use and the product being passed thru the car api and users api.

--- server.ts
+++ server.ts
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/users',usersRoute )
-//app.use("/products", productsRoute)
+app.use("/products", productsRoute)
 //app.use("/bills",billsRoute)
 // app.get('/bills', (req: express.Request, res: express.Response)=> {
 //     res.send('This are THE BILLS!');
