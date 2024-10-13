ca048fccb667c2fcf789a5ddb7803801dceace8a
kreativesahara <technologieslensmen@gmail.com>
2024-10-13 16:01:18 +0300

    Fixed: The get method of a single user on the usercontroller

--- db/schema/user.ts
+++ db/schema/user.ts
@@ -7,7 +7,7 @@ import { int, varchar, datetime, mysqlTable } from 'drizzle-orm/mysql-core';
 // User Schema
 //++++++++++++++++++++++++++++++++++++++++++
 export const users = mysqlTable('users', {
-    _id: int('id').primaryKey().autoincrement(),
+    id: int('id').primaryKey().autoincrement(),
     firstname: varchar('first_name', { length: 255 }),
     lastname: varchar('last_name', { length: 255 }),
     email: varchar('email', { length: 255 }),
