cc0adaf67c0d875c7de5ece2fa49eebb347b6fea
kreativsahara <technologieslensmen@gmail.com>
2025-02-12 15:47:07 +0300

    Modified: Data routing for the Products controller was fixed and added role verification for the products requests from the backed that is at the put and delete method.

--- routes/Api/product.ts
+++ routes/Api/product.ts
@@ -3,13 +3,13 @@ import { ROLES_LIST } from "../../config/roles_list";
 import { verifyRoles } from "../../middleware/verifyRoles";
 
 const router = Router();
-const { getAllProducts, getProduct, createProduct, updateSingleProduct,deleteProduct } = require("../../controllers/productsController");
+const { getAllProducts, getProduct, createProduct, updateUpload, deleteUpload } = require("../../controllers/productsController");
 
 router.route("/")
     .get(verifyRoles(ROLES_LIST.Visitor,ROLES_LIST.Member,ROLES_LIST.Seller,ROLES_LIST.Modarator,ROLES_LIST.Admin),getAllProducts)
     .post(verifyRoles(ROLES_LIST.Visitor, ROLES_LIST.Member, ROLES_LIST.Seller, ROLES_LIST.Modarator, ROLES_LIST.Admin), createProduct)
-    // .put(updateSingleProduct)
-    // .delete(deleteProduct);
+    .put(verifyRoles(ROLES_LIST.Visitor, ROLES_LIST.Member, ROLES_LIST.Seller, ROLES_LIST.Modarator, ROLES_LIST.Admin), updateUpload)
+    .delete(verifyRoles(ROLES_LIST.Visitor, ROLES_LIST.Member, ROLES_LIST.Seller, ROLES_LIST.Modarator, ROLES_LIST.Admin), deleteUpload);
 router.route("/:id")
     .get(verifyRoles(ROLES_LIST.Visitor, ROLES_LIST.Member, ROLES_LIST.Seller, ROLES_LIST.Modarator, ROLES_LIST.Admin), getProduct)
 export default router;
