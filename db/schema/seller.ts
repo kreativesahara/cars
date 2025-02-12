f9453650e57a7220f423d781a12695bdaf4f7a0e
kreativsahara <technologieslensmen@gmail.com>
2025-02-12 15:35:12 +0300

    Seller Schema to all implementation of the seller controller such that a members account can be upgrade to sellers account type

--- db/schema/seller.ts
+++ db/schema/seller.ts
@@ -1,12 +1,15 @@
-import { int, varchar, datetime, mysqlTable } from 'drizzle-orm/mysql-core';
+import { int, text, varchar, datetime, mysqlTable } from 'drizzle-orm/mysql-core';
+import { user } from './user';
 
 export const seller = mysqlTable('sellers', {
-    userId: int('user_id').primaryKey().autoincrement(),
-    username: varchar('username', { length: 255 }),
+    Id: int('id').primaryKey().autoincrement(),
+    // userId: int('user_id').notNull().references(() => user.id),
+    userId: int('user_id').notNull(),
+    username: text('username').notNull(),
     accountType: varchar('account_type', { length: 255 }),
     contact: varchar('contact', { length: 50 }),
     //phone_no: varchar('phone_no', { length: 255 }),
-    place: int('place').notNull(),
+    place: varchar('place', { length: 200 }).notNull(),
     // subscription: int('subscription_type'),
     hasFinancing: varchar('has_financing', { length: 255 }),
     acceptsTradeIn: varchar('accepts_trade_in', { length: 255 }),
