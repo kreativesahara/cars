d0273466630fe855177c1a53dcffa12e27351869
kreativsahara <technologieslensmen@gmail.com>
2025-02-12 15:34:37 +0300

    Modified: Seller Schema to all implementation of the seller controller such that a members account can be upgrade to sellers account type

--- routes/Api/seller.ts
+++ routes/Api/seller.ts
@@ -1,15 +1,17 @@
 import { Router } from "express";
+import { ROLES_LIST } from "../../config/roles_list";
+import { verifyRoles } from "../../middleware/verifyRoles";
 
 const router = Router();
-const { getAllSellers, createUser, getUser, updateUser, deleteUser } = require("../../controllers/sellersController");
+const { getAllSellers, createSeller, getSeller, updateSeller, deleteSeller } = require("../../controllers/sellersController");
 
 router.route("/")
-    .get(getAllSellers)
-    // .post(createUser)
-    // .put(updateUser)
-    // .delete(deleteUser);
+    .post(verifyRoles(ROLES_LIST.Visitor, ROLES_LIST.Member, ROLES_LIST.Seller, ROLES_LIST.Modarator, ROLES_LIST.Admin), createSeller)
+    .get(verifyRoles(ROLES_LIST.Visitor, ROLES_LIST.Member, ROLES_LIST.Seller, ROLES_LIST.Modarator, ROLES_LIST.Admin), getAllSellers)
+    .put(verifyRoles(ROLES_LIST.Visitor, ROLES_LIST.Member, ROLES_LIST.Seller, ROLES_LIST.Modarator, ROLES_LIST.Admin), updateSeller)
+    .delete(verifyRoles(ROLES_LIST.Visitor, ROLES_LIST.Member, ROLES_LIST.Seller, ROLES_LIST.Modarator, ROLES_LIST.Admin), deleteSeller);
 
 router.route("/:id")
-    // .get(getUser)
+    .get(verifyRoles(ROLES_LIST.Visitor, ROLES_LIST.Member, ROLES_LIST.Seller, ROLES_LIST.Modarator, ROLES_LIST.Admin), getSeller)
 
 export default router;
