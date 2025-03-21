3dab9976ca45790e7e6043281d594511faa8b3b7
kreativsahara <technologieslensmen@gmail.com>
2025-03-22 00:56:56 +0300

    Fixed: addProduct and itempage for proper ui

--- client/src/itemPage.jsx
+++ client/src/itemPage.jsx
@@ -77,7 +77,13 @@ const Itempage = () => {
                             alt={product.make}
                         />
                     </div>
-                    <div className={`grid grid-cols-${Math.min(product.images.length, 6)} py-2 px-1 rounded-b gap-2 cursor-pointer`}>
+                    <div className={`grid ${product.images.length === 1 ? "grid-cols-1" :
+                            product.images.length === 2 ? "grid-cols-2" :
+                                product.images.length === 3 ? "grid-cols-3" :
+                                    product.images.length === 4 ? "grid-cols-4" :
+                                         "grid-cols-5"} 
+                                        py-2 px-1 rounded-b gap-2 cursor-pointer`
+                         }>
                         {product.images.map((img, index) => (
                             <img
                                 key={index}
