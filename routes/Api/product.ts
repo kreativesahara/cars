b7b7f0ec3c7592eda37afb1f1e1fa787a831192f
kreativsahara <technologieslensmen@gmail.com>
2025-03-24 22:27:15 +0300

    Modified: Changed The requested params for productID to slug on the server router for products

--- routes/Api/product.ts
+++ routes/Api/product.ts
@@ -8,7 +8,7 @@ const { getAllProducts, getProduct, createProduct, updateUpload, deleteUpload } 
 router.route("/")
     .get(verifyRoles(ROLES_LIST.Visitor, ROLES_LIST.Member, ROLES_LIST.Seller, ROLES_LIST.Modarator, ROLES_LIST.Admin) ,getAllProducts)
     .post(verifyRoles(ROLES_LIST.Visitor, ROLES_LIST.Member, ROLES_LIST.Seller, ROLES_LIST.Modarator, ROLES_LIST.Admin), createProduct)
-router.route("/:id")
+router.route("/:slug")
     .get(verifyRoles(ROLES_LIST.Visitor, ROLES_LIST.Member, ROLES_LIST.Seller, ROLES_LIST.Modarator, ROLES_LIST.Admin),getProduct)
     .put(verifyRoles(ROLES_LIST.Visitor, ROLES_LIST.Member, ROLES_LIST.Seller, ROLES_LIST.Modarator, ROLES_LIST.Admin), updateUpload)
     .delete(verifyRoles(ROLES_LIST.Visitor, ROLES_LIST.Member, ROLES_LIST.Seller, ROLES_LIST.Modarator, ROLES_LIST.Admin), deleteUpload);
