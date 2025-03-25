6ff6c77b7050a4e0d2d69008d28b41efe0a00ac5
kreativsahara <technologieslensmen@gmail.com>
2025-03-25 14:43:56 +0300

    Added: A new column by image_url on the schema to store seller profile

--- db/schema/seller.ts
+++ db/schema/seller.ts
@@ -1,4 +1,4 @@
-import { int, text, varchar, datetime, mysqlTable } from 'drizzle-orm/mysql-core';
+import { int, text, varchar, timestamp, mysqlTable } from 'drizzle-orm/mysql-core';
 import { user } from './user';
 
 export const seller = mysqlTable('sellers', {
@@ -8,10 +8,10 @@ export const seller = mysqlTable('sellers', {
     username: text('username').notNull(),
     accountType: varchar('account_type', { length: 255 }),
     contact: varchar('contact', { length: 50 }),
-    //phone_no: varchar('phone_no', { length: 255 }),
     place: varchar('place', { length: 200 }).notNull(),
-    // subscription: int('subscription_type'),
+    image_url: varchar('image_url', { length: 255 }).notNull(),
     hasFinancing: varchar('has_financing', { length: 255 }),
     acceptsTradeIn: varchar('accepts_trade_in', { length: 255 }),
-    //created_at: datetime('created_at'),
+    created_at: timestamp('created_at').defaultNow().notNull(),
+   
 });
