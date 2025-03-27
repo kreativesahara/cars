e6e6fb68ca1191a3e311481bd18a98a90d18c316
kreativsahara <technologieslensmen@gmail.com>
2025-03-27 19:45:06 +0300

    Modified: The component that shows the users subscription status on the dashboard

---
+++ client/src/components/cards/subscriptionDetailsCard.jsx
@@ -0,0 +1,9 @@
+export default function subscription() {
+    return (
+        <div className='p-4 bg-neutral-50 rounded-md flex flex-col gap-2'>
+            <h2 className='text-lg font-medium text-neutral-950'>Subscription Details</h2>
+            <p className='text-sm text-neutral-600'>Plan: Pro Membership</p>
+            <p className='text-sm text-neutral-600'>Renewal Date: 2023-12-05</p>
+        </div>
+    )
+}
