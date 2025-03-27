0b67915e2ec08960c42a949a10ffb8663546b61d
kreativsahara <technologieslensmen@gmail.com>
2025-03-27 19:41:10 +0300

    Modified: Subscription schema to manage subscription status

--- db/schema/subscription.ts
+++ db/schema/subscription.ts
@@ -4,10 +4,11 @@ import { user } from './user'; // Import the user table
 export const subscription = mysqlTable('subscriptions', {
     id: int('id').primaryKey().autoincrement(),
     userId: int('user_id').notNull().references(() => user.id, { onDelete: 'cascade' }),
+    txnId: varchar("txn_id", { length: 255 }).unique(), // Unique transaction reference
     planName: varchar('plan_name', { length: 50 }).notNull(), // Basic, Pro, 
     amount: decimal('amount', { precision: 10, scale: 2 }).notNull(), // Price in KES
     currency: varchar('currency', { length: 10 }).notNull().default('KES'),
-    status: varchar('status', { length: 20 }).notNull().default('active'), // active, cancelled, expired
+    status: varchar('status', { length: 20 }).notNull().default('pending'), // pending,active, cancelled, expired
     startDate: timestamp('start_date').defaultNow(),
     endDate: timestamp('end_date'),
     createdAt: timestamp('created_at').defaultNow().notNull(),
