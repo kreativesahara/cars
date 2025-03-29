55441b0a4e7af0a34b79be0608705fa8665c7381
kreativsahara <technologieslensmen@gmail.com>
2025-03-30 02:50:52 +0300

    Modified: Remove old DB connection file and integrate connection logic into dbConfig for improved structure

--- db/dbConfig.ts
+++ db/dbConfig.ts
@@ -1,11 +1,33 @@
-//++++++++++++++++++++++++++++++++++++++++++
-//Import
-//++++++++++++++++++++++++++++++++++++++++++
-import { drizzle } from "drizzle-orm/mysql2";
-import connection from "../config/connect";
-//++++++++++++++++++++++++++++++++++++++++++
-const db = drizzle(connection);
-//++++++++++++++++++++++++++++++++++++++++++
-export default db;
-//++++++++++++++++++++++++
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+import { drizzle } from 'drizzle-orm/mysql2';
 
+dotenv.config();
+//++++++++++++++++++++++++++++++++++++++++++
+// DB Connection
+//++++++++++++++++++++++++++++++++++++++++++
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+//++++++++++++++++++++++++++++++++++++++++++
+// DB Connection Test
+//++++++++++++++++++++++++++++++++++++++++++
+connection.connect((err) => {
+    if (err) {
+        console.log('There Is Error In DB Connection:' + err);
+    }
+    else {
+        console.log('DB Connected Successfully')
+    }
+})
+
+const db = drizzle(connection)
+///export default connection;
+export default db;
+//++++++++++++++++++++++++++++++++++++++++++    
